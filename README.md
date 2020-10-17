c510cacd4dbd386478abdf32e5b0f968ee5c0d8a
Prabh Singh <prabhm512@gmail.com>
2020-10-17 13:14:25 +1100

    licenses rendered as badges

--- README.md
+++ README.md
@@ -1,38 +1,38 @@
-# sdfs
+# sdf
             
 ## Description
 
-dfsdf
+dsfsdf
 
 ## Table of Contents
 
 [Installation](#-installation)  
 [Usage](#-usage)  
 [License](#-license)  
-[Contributing](#-contributing)
-[Tests](#-tests) 
+[Contributing](#-contributing)                                             
+[Tests](#-tests)                                                                    
 [Questions](#-questions) 
 
 ## Installation
 
-df
+sdf
 
 ## Usage
 
-df
+sdf
 
 ## License
 
-GNU AGPLv3,MIT License 
+[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0) [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0) [![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt) 
 
 ## Contributing
 
-sd
+ass
 
 ## Tests
 
-<a href="Develop\index.js">Develop\index.js</a>
+<a href="sd">sd</a>
 
 ## Questions 
 
-For any questions regarding this project, please email dsf.
+For any questions regarding this project, please email sdf.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,54 @@
+// Store badge url of selected licenses
+let selectedLicenses = [];
+
+// License badges
+let licenseBadge = [
+  {
+    name: "GNU AGPLv3",
+    url: "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)"
+  },
+  {
+    name: "GNU GPLv3",
+    url: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+  },
+  {
+    name: "GNU LGPLv3",
+    url: "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)"
+  },
+  {
+    name: "MIT License",
+    url: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  },
+  {
+    name: "Mozilla Public License 2.0",
+    url: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  },
+  {
+    name: "Apache License 2.0",
+    url: "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  },
+  {
+    name: "Boost Software License 1.0",
+    url: "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+  },
+  {
+    name: "The Unlicense",
+    url: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+  }
+]
+
 // function to generate markdown for README
 function generateMarkdown(data) {
+
+  // Convert license name into badge
+  for (let i=0; i<data.license.length; i++) {;
+    for (let j=0; j<licenseBadge.length; j++) {
+      if (data.license[i] == licenseBadge[j].name) {
+        selectedLicenses.push(licenseBadge[j].url);
+      }
+    }
+  }
+
   return `# ${data.title}
             
 ## Description
@@ -11,8 +60,8 @@ ${data.desc}
 [Installation](#-installation)  
 [Usage](#-usage)  
 [License](#-license)  
-[Contributing](#-contributing)
-[Tests](#-tests) 
+[Contributing](#-contributing)                                             
+[Tests](#-tests)                                                                    
 [Questions](#-questions) 
 
 ## Installation
@@ -25,7 +74,7 @@ ${data.usage}
 
 ## License
 
-${data.license} 
+${selectedLicenses.join(" ")} 
 
 ## Contributing
 
